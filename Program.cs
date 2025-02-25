2d1481c5a86145fac066b92d74f2ac46159ec899
Kaue Piovan <kaupiovan@gmail.com>
2025-02-24 23:42:32 -0300

    Menu function

--- Program.cs
+++ Program.cs
@@ -1,4 +1,8 @@
-﻿namespace TextEditor;
+﻿using System.ComponentModel.DataAnnotations;
+using System.Runtime.InteropServices.Swift;
+using Microsoft.VisualBasic;
+
+namespace TextEditor;
 
 class Program
 {
@@ -6,4 +10,29 @@ class Program
     {
         Console.WriteLine("Hello, World!");
     }
+
+    static void Menu()
+    {
+        Console.Clear();
+        Console.WriteLine("O que deseja fazer? ");
+        Console.WriteLine("1 - Abrir arquivo.");
+        Console.WriteLine("2 - Criar novo arquivo.");
+        Console.WriteLine("0 - Sair.");
+
+        short option = short.Parse(Console.ReadLine());
+
+        switch (option)
+        {
+            case 0: System.Environment.Exit(0); break;
+            case 1: Open(); break;
+            case 2: Edit(); break;
+            default: Menu(); break;
+        }
+    }
+
+    static void Open()
+    {}
+
+    static void Edit()
+    {}
 }
